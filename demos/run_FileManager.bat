9477b0f8270b508925ae0134ed0ccd7fc130146f
washheart@163.com <opendoor>
2015-07-31 16:35:06 +0800

    修改fastjson工具包：在JSONSerializer中，有时获取到的线程classloader为null，此时取JSONSerializer自己的

--- demos/run_FileManager.bat
+++ demos/run_FileManager.bat
@@ -3,6 +3,6 @@ if not exist JAVA_HOME set JAVA_HOME=d:\java\jdk1.6.0_24
 set path=%JAVA_HOME%\bin\
 set rootpath=%cd%\\..
 
-set classpath= %rootpath%\\JWebTop_J\\bin;%rootpath%\\demos\FileManager\\bin;%rootpath%\\JWebTop_J\\lib\\jackson-core-2.4.0.jar;%rootpath%\\JWebTop_J\\lib\\fastjson-1.1.28.jar;
+set classpath= %rootpath%\\JWebTop_J\\bin;%rootpath%\\demos\FileManager\\bin;%rootpath%\\JWebTop_J\\lib\\jackson-core-2.4.0.jar;%rootpath%\\JWebTop_J\\lib\\fastjson-1.1.28-x.jar;
 cd %rootpath%\\jwebtop_c\\Release
 java -classpath %classpath% org.jwebtop.demos.fm.FileManager
