94b86eb86b41c17d317b23ac9e42edcc301b6ad5
washheart@163.com <opendoor>
2015-08-06 11:19:34 +0800

    刚才的没有处理非multiloop的情况

--- JWebTop_c/JWebTop/JWebTop_main.cpp
+++ JWebTop_c/JWebTop/JWebTop_main.cpp
@@ -108,7 +108,7 @@ int startJWebTop(HINSTANCE hInstance/*当前应用的实例*/, LPTSTR appDefFile, long 
 	}
 	else{
 		CefInitialize(main_args, settings, app.get(), sandbox_info);// 初始化cef
-		//CefRunMessageLoop();// 运行CEF消息监听，知道CefQuitMessageLoop()方法被调用
+		CefRunMessageLoop();// 运行CEF消息监听，知道CefQuitMessageLoop()方法被调用
 		// 下面的这种是自己侦听消息循环的方式，性能比CefRunMessageLoop()略差，但是好处是能自己控制线程间的锁定（但是依然无法解决java线程与cef消息冲突的问题）
 		//while (runable){
 		//	CefDoMessageLoopWork();
