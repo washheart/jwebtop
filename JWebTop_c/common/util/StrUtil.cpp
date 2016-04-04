ed852dcc9f28e2cf78c70436273c5ef6a35e6169
washheart@163.com <opendoor>
2016-04-04 17:41:21 +0800

    修改生成的UUID，必要减号等分隔符号

--- JWebTop_c/common/util/StrUtil.cpp
+++ JWebTop_c/common/util/StrUtil.cpp
@@ -259,7 +259,8 @@ namespace jw{
 		CoInitialize(NULL);
 		if (S_OK == CoCreateGuid(&guid))
 		{
-			_snprintf(guidBuf, 64, "%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X",
+			//_snprintf(guidBuf, 64, "%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X",
+			_snprintf(guidBuf, 64, "%08X%04X%04X%02X%02X%02X%02X%02X%02X%02X%02X",
 				guid.Data1, guid.Data2, guid.Data3,
 				guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3],
 				guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]);
