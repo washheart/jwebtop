40ef79dd0431593b25ed1e62a789dda3f38d5951
washheart <washheart@163.com>
2016-07-22 10:31:12 +0800

    默认配置中把gup给关闭

--- JWebTop_c/JWebTop/browser/JWebTopApp.cpp
+++ JWebTop_c/JWebTop/browser/JWebTopApp.cpp
@@ -10,7 +10,7 @@ namespace jw{
 void JWebTopApp::OnBeforeCommandLineProcessing(
 	const CefString& process_type,
 	CefRefPtr<CefCommandLine> command_line) {
-	// command_line->AppendSwitch("--disable-gpu");// 禁止gpu（开启gpu会增加一个新进程，而且在有的电脑上反而会降低性能）
+	command_line->AppendSwitch("--disable-gpu");// 禁止gpu（开启gpu会增加一个新进程，而且在有的电脑上反而会降低性能）
 	// command_line->AppendSwitch("--allow-file-access-from-files");
 	// command_line->AppendSwitch("--disable-web-security");
 	// command_line->AppendSwitch("--allow-file-access");
